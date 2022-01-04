d8756596e6803acbea4074e3c06953fcff94fa59
DrHowdyDoo <55204060+DrHowdyDoo@users.noreply.github.com>
2022-01-04 11:02:19 +0530

    Small UI improvements

--- app/src/main/java/com/drhowdydoo/diskinfo/Util.java
+++ app/src/main/java/com/drhowdydoo/diskinfo/Util.java
@@ -1,13 +1,19 @@
 package com.drhowdydoo.diskinfo;
 
+import java.text.DecimalFormat;
+
 public class Util {
 
     public static String FormatBytes(long bytes) {
 
+        double bytesInDouble = (double) bytes;
+        DecimalFormat df = new DecimalFormat("#.#");
+
         if (bytes < 1024) return bytes + " Bytes";
-        else if (bytes / 1024 < 1024) return (bytes / 1024) + " KB";
-        else if (bytes / (1024 * 1024) < 1024) return (bytes / (1024 * 1024)) + " MB";
-        else return (bytes / (1024 * 1024 * 1024)) + " GB";
+        else if (bytes / 1024 < 1024) return df.format((bytesInDouble / 1024.0)) + " KB";
+        else if (bytes / (1024 * 1024) < 1024)
+            return df.format((bytesInDouble / (1024.0 * 1024.0))) + " MB";
+        else return df.format((bytesInDouble / (1024.0 * 1024.0 * 1024.0))) + " GB";
     }
 
 
